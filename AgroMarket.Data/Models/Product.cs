c9fd729e66609451e6d627deab98bf43b8c0d649
Ashish Neupane <47056442+Unicodist@users.noreply.github.com>
2022-07-26 17:10:34 +0545

    Modified product model

--- AgroMarket.Data/Models/Product.cs
+++ AgroMarket.Data/Models/Product.cs
@@ -9,7 +9,14 @@ namespace AgroMarket.Data.Models
     public class Product
     {
         public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
         public long ProductClassId { get; set; }
-        public ProductClass Class { get; set; }
+        public long FarmerId { get; set; }
+        public virtual ProductClass Class { get; set; }
+        public virtual User Farmer { get; set; }
     }
 }
