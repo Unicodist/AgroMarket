17c53a5cb52adf4819dd21c8efe4f313e1e17bc0
Sneha849 <snehapokharel10@gmail.com>
2022-07-26 16:48:28 +0545

    configuration added

---
+++ AgroMarket.Data/Mapping/CartConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AgroMarket.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroMarket.Data.Mapping
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            _ = builder.ToTable("cart");
+            _ = builder.HasKey(a => a.Id);
+            _ = builder.Property(a => a.Id).HasColumnName("cart_id");
+
+            
+        }
+    }
+}
---
+++ AgroMarket.Data/Mapping/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AgroMarket.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroMarket.Data.Mapping
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            _ = builder.ToTable("order");
+            _ = builder.HasKey(a => a.Id);
+            _ = builder.Property(a => a.Id).HasColumnName("order_id");
+
+
+        }
+    }
+}
