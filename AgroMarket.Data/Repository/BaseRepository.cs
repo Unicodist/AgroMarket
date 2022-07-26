ef734c48e1dd1aa8371d2fa2208fd5ca126c35c5
Ashish Neupane <47056442+Unicodist@users.noreply.github.com>
2022-07-26 15:24:47 +0545

    Added repository

---
+++ AgroMarket.Data/Repository/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AgroMarket.Data.Repository;
+
+public class BaseRepository<T> where T : class
+{
+    private AmContext _context;
+    private DbSet<T> _set;
+    public BaseRepository(AmContext context)
+    {
+        _context = context;
+        _set = context.Set<T>();
+    }
+
+    public void InsertAsync(T entity)
+    {
+        _ = _context.AddAsync(entity);
+        _context.SaveChangesAsync();
+    }
+}
---
+++ AgroMarket.Data/Repository/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using AgroMarket.Data.Models;
+
+namespace AgroMarket.Data.Repository;
+
+public class UserRepository : BaseRepository<User>
+{
+    public UserRepository(AmContext context) : base(context)
+    {
+    }
+}
