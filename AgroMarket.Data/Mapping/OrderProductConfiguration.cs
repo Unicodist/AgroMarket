58e0ccefee42227e5d1db8d068b51a3f8c1d27b7
Sneha849 <snehapokharel10@gmail.com>
2022-07-27 17:39:53 +0545

    orderproductconfig added

---
+++ AgroMarket.Data/Mapping/OrderProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AgroMarket.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AgroMarket.Data.Mapping
+{
+    public class OrderProductConfiguration : IEntityTypeConfiguration<OrderProduct>
+    {
+        public void Configure(EntityTypeBuilder<OrderProduct> builder)
+        {
+            _ = builder.ToTable("orderproduct");
+            _ = builder.HasKey(a => a.OrderId);
+            _ = builder.Property(a => a.OrderId).HasColumnName("orderproduct_orderid");
+            _ = builder.Property(a => a.ProductId).HasColumnName("orderproduct_productid");
+
+            _ = builder.HasOne(a => a.Order).WithMany().HasForeignKey(a => a.OrderId);
+            _ = builder.HasOne(a => a.Product).WithMany().HasForeignKey(a => a.ProductId);
+        }
+
+    }
+}
--- AgroMarket.Data/Mapping/ProductConfiguration.cs
+++ AgroMarket.Data/Mapping/ProductConfiguration.cs
@@ -11,7 +11,7 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         _ = builder.ToTable("product");
         _ = builder.HasKey(a => a.Id);
         _ = builder.Property(a => a.Id).HasColumnName("product_id");
-        _ = builder.Property(a => a.ProductClassId).HasColumnName("product_id");
+        _ = builder.Property(a => a.ProductClassId).HasColumnName("product_productclassid");
 
         _ = builder.HasOne(a => a.Class).WithMany(a => a.Products).HasForeignKey(a=>a.ProductClassId);
     }
