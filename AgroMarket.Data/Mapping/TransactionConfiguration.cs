3191d824b10a26835f403324ae5e7f12b79a43a3
Sneha849 <snehapokharel10@gmail.com>
2022-07-27 16:32:17 +0545

    changes in transactioncofig

--- AgroMarket.Data/Mapping/TransactionConfiguration.cs
+++ AgroMarket.Data/Mapping/TransactionConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using AgroMarket.Data.Models;
+﻿using AgroMarket.Data.Converters;
+using AgroMarket.Data.Enum;
+using AgroMarket.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -11,7 +13,12 @@ namespace AgroMarket.Data.Mapping
             _ = builder.ToTable("transaction");
             _ = builder.HasKey(a => a.Id);
             _ = builder.Property(a => a.Id).HasColumnName("transaction_id");
-            
+            _ = builder.Property(a => a.State).HasColumnName("transaction_state").HasConversion(new EnumConverter<ShipmentState>());
+
+            _ = builder.Property(a => a.OrderId).HasColumnName("order_id");
+
+            _ = builder.HasOne(a => a.Order).WithMany().HasForeignKey(a => a.OrderId);
+
         }
     }
 }
