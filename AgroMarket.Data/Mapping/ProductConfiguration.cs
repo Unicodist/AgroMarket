d2ed050075a4eaedae7223903f191a6d59c033bd
Sneha849 <snehapokharel10@gmail.com>
2022-07-27 17:54:08 +0545

    prodconfigadded

--- AgroMarket.Data/Mapping/ProductConfiguration.cs
+++ AgroMarket.Data/Mapping/ProductConfiguration.cs
@@ -11,8 +11,19 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
         _ = builder.ToTable("product");
         _ = builder.HasKey(a => a.Id);
         _ = builder.Property(a => a.Id).HasColumnName("product_id");
-        _ = builder.Property(a => a.ProductClassId).HasColumnName("product_productclassid");
+
+        _ = builder.Property(a => a.ProductClassId).HasColumnName("productclass_id");
 
         _ = builder.HasOne(a => a.Class).WithMany(a => a.Products).HasForeignKey(a=>a.ProductClassId);
+
+        _ = builder.Property(a => a.Name).HasColumnName("product_name");
+        _ = builder.Property(a => a.Description).HasColumnName("product_description");
+        _ = builder.Property(a => a.ExpiryDate).HasColumnName("product_expirydate");
+        _ = builder.Property(a => a.Price).HasColumnName("product_price");
+        _ = builder.Property(a => a.Stock).HasColumnName("product_stock");
+
+        _ = builder.Property(a => a.FarmerId).HasColumnName("farmer_id");
+
+        _ = builder.HasOne(a => a.Farmer).WithMany().HasForeignKey(a => a.FarmerId);
     }
 }
