d2391f83cddeb5145cc2eec765ab6d71ae306423
Ashish Neupane <47056442+Unicodist@users.noreply.github.com>
2022-07-29 09:41:35 +0545

    Added dependency injection

---
+++ AgroMarket/DiConfig.cs
@@ -0,0 +1,17 @@
+﻿using AgroMarket.Data;
+using AgroMarket.Data.Repository;
+using AgroMarket.Service;
+
+namespace AgroMarket;
+
+public static class DiConfig
+{
+    public static void DiConf(this IServiceCollection service)
+    {
+        service.AddSingleton<AmContext>();
+
+        service.AddSingleton<UserService>();
+
+        service.AddSingleton<UserRepository>();
+    }
+}
