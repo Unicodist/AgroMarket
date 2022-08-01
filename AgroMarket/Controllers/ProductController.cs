d40497f83c94615b485e082212506de36cbf4675
Sneha849 <snehapokharel10@gmail.com>
2022-08-01 12:59:51 +0545

    changes in product

--- AgroMarket/Controllers/ProductController.cs
+++ AgroMarket/Controllers/ProductController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgroMarket.Controllers
 {
     public class ProductController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+        [AllowAnonymous]
         public IActionResult Recent()
         {
             return View();
