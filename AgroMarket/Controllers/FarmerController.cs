859c0f1e3f7d233e7de0226f657d8a4b54b4f520
Sneha849 <snehapokharel10@gmail.com>
2022-08-08 12:59:42 +0545

    added farmer layout

---
+++ AgroMarket/Controllers/FarmerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroMarket.Controllers
+{
+    public class FarmerController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AgroMarket/Views/Farmer/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgroMarket.Views.Farmer
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
