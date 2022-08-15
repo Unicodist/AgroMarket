6c415fceaedf362373085af479864c1f0b012148
Ashish Neupane <ashishneupane999@gmail.com>
2022-08-15 16:28:13 +0545

    Potential fix on latest products

--- AgroMarket/wwwroot/js/Home/index.js
+++ AgroMarket/wwwroot/js/Home/index.js
@@ -1,5 +1,11 @@
 ﻿window.addEventListener('load',()=>{
    $.get('/api/productapi/getlatestproducts',function (data){
-      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+         $('#categorized_proudct_wrapper').append(renderCategorizedProductCard(data[i].categoryName));
+         for (let j = 0; j < data[i].products.length; j++) {
+            let holderid = data[i].categoryName+'_card_wrapper';
+            document.getElementById(holderid).innerHTML+=renderMyProductItemCard(data[i].products[j].name,data[i].products[j].price,data[i].products[j].image);
+         }
+      }
    });
 });
